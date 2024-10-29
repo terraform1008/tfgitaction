4e9c9a261bdf81bd06f036b964615f5e75b01f36
Arun <terraform1008@outlook.com>
2024-10-29 13:29:33 +0000

    create rerouce group

--- src/main.tf
+++ src/main.tf
@@ -22,7 +22,8 @@ provider "azurerm" {
 }
 
 # Define any Azure resources to be created here. A simple resource group is shown here as a minimal example.
-resource "azurerm_resource_group" "rg-aks" {
-  name     = var.resource_group_name
-  location = var.location
+# Create a resource group
+resource "azurerm_resource_group" "terra-git-action" {
+  name     = "terragitaction"
+  location = "Canada Central"
 }
